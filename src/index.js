8cccb97da5b67abb33bf9125ce379273125b8446
Weijie Jiang <weijiej2964@hstat.org>
2022-02-18 15:03:26 +0000

    update repo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import{ initializeApp }from 'firebase/app'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZQhVb2pn-m2Z4RzTPizRBlZCdqL6bcvo",
+  authDomain: "infocards-71e92.firebaseapp.com",
+  projectId: "infocards-71e92",
+  storageBucket: "infocards-71e92.appspot.com",
+  messagingSenderId: "943364574192",
+  appId: "1:943364574192:web:c68d516ba528285bc12b35"
+};
